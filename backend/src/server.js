7a9f20ac50ecac03ca27fb930305a41d36c74534
gitFa07 <office.falakanwar@gmail.com>
2026-06-22 12:53:07 +0530

    Adding cors in backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -10,8 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5001
 
 // middleware
+app.use(cors(
+    { origin: "http://localhost:5173", }
+));
 app.use(express.json());
 app.use(rateLimiter);
+
 // app.use((req, res, next)=> {
 //     console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
 //     next();
