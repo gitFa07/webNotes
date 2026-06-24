08770e2cc9f296b38aba05196f084373d770c890
gitFa07 <office.falakanwar@gmail.com>
2026-06-24 16:28:53 +0530

    Corrected Date Representation & bg-color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
-import CreatePage from './pages/CreatePage';
-import NoteDetailPage from './pages/NoteDetailPage';
-import toast from 'react-hot-toast';
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div data-theme="aqua">
+    <div className="bg-base-200">
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/create' element={<CreatePage/>} />
-        <Route path='/note/:id' element={<NoteDetailPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  ); 
+  );
 };
 
 export default App;
